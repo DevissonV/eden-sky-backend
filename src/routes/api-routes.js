29ce85f2ed42d4bbc086a76c151313571a06ffbe
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-12-21 10:21:17 -0500

    adds endpoints for health check

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import employeeRoutes from './employee-routes.js';
 import requestRoutes from './request-routes.js';
 import userRoutes from './user-routes.js';
+import testRoutes from './health-check-routes.js'
 
 const apiRoutes = Router();
 
 apiRoutes.use('/employees', employeeRoutes);
 apiRoutes.use('/requests', requestRoutes);
 apiRoutes.use('/users', userRoutes);
+apiRoutes.use('/health-checks', testRoutes);
 
 export default apiRoutes;
 
