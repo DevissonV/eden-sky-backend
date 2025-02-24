41c5c94cff9bf201938d193420c85eba9b162e0e
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-02-24 12:12:36 -0500

    feat: enhance employee and request tests with user registration and cleanup; improve test descriptions

--- tests/employees.spec.js
+++ tests/employees.spec.js
@@ -5,16 +5,33 @@ describe('Employee API', () => {
   let token;
   let createdEmployeeId;
 
-  beforeAll(async () => {
+  const registerAndLoginUser = async () => {
+    await request(app).post('/api/users/register').send({
+      username: 'userCreatedForTesting',
+      password: 'securepassword123',
+      role: 'admin',
+    });
+
     const loginResponse = await request(app).post('/api/users/login').send({
-      username: 'admin1',
+      username: 'userCreatedForTesting',
       password: 'securepassword123',
     });
 
-    token = loginResponse.body.data.token;
+    return loginResponse.body.data.token;
+  };
+
+  beforeAll(async () => {
+    token = await registerAndLoginUser();
   });
 
-  it('Create a new employee', async () => {
+  afterAll(async () => {
+    await request(app)
+      .delete('/api/users/')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ username: 'userCreatedForTesting' });
+  });
+
+  it('should create a new employee', async () => {
     const employeeData = {
       name: 'Devisson',
       hire_date: '2025-02-01',
@@ -28,11 +45,10 @@ describe('Employee API', () => {
 
     expect(response.status).toBe(201);
     expect(response.body.data[0]).toHaveProperty('name', employeeData.name);
-
     createdEmployeeId = response.body.data[0].id;
   });
 
-  it('Get all employees', async () => {
+  it('should get all employees', async () => {
     const response = await request(app)
       .get('/api/employees/')
       .set('Authorization', `Bearer ${token}`);
@@ -42,7 +58,7 @@ describe('Employee API', () => {
     expect(Array.isArray(response.body.data)).toBe(true);
   });
 
-  it('Get a single employee by ID', async () => {
+  it('should get a single employee by ID', async () => {
     const response = await request(app)
       .get(`/api/employees/${createdEmployeeId}`)
       .set('Authorization', `Bearer ${token}`);
@@ -51,7 +67,7 @@ describe('Employee API', () => {
     expect(response.body.data).toHaveProperty('id', createdEmployeeId);
   });
 
-  it('Update an employee', async () => {
+  it('should update an employee', async () => {
     const updatedData = {
       name: 'Dev',
       hire_date: '2025-03-01',
@@ -67,7 +83,7 @@ describe('Employee API', () => {
     expect(response.body.data[0]).toHaveProperty('name', updatedData.name);
   });
 
-  it('Delete an employee', async () => {
+  it('should delete an employee', async () => {
     const response = await request(app)
       .delete(`/api/employees/${createdEmployeeId}`)
       .set('Authorization', `Bearer ${token}`);
